d349c045df0a19620760475555b71d45977fdbb0
Yididya <jedidiahandualem@gmail.com>
2023-02-22 19:55:49 +0300

    task-0-runner

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+    Inherit from BaseCache and perform get and put op
+"""
+
+BasicCaching = __import__('0-basicCache').BaseCaching
+
+class BasicCache(BasicCaching):
+    """
+    Put and get methods to deal with cache
+    """
+    def put(self, key, item):
+        self.cache_data[key] = item
+
+    def get(self, key):
+        if key in self.cache_data.keys() and key:
+            return self.cache_data[key]
+        return None
