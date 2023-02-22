a073b51d5104c4f520e5331623aa225289100749
Yididya <jedidiahandualem@gmail.com>
2023-02-22 20:43:31 +0300

    [fix]-task--0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,16 +10,24 @@ BasicCaching = __import__('0-basicCache').BaseCaching
 class BasicCache(BasicCaching):
     """
     Put and get methods to deal  with cache
+    Methods
+        put
+        get
     """
     def __init__(self):
         """
         init the object and call parent class
+        Args
+            self
         """
         super().__init__()
 
     def put(self, key, item):
         """
         insert items to parent class attribute
+        Args
+            key
+            item
         """
         if key and item:
             self.cache_data[key] = item
@@ -27,6 +35,8 @@ class BasicCache(BasicCaching):
     def get(self, key):
         """
         return item from dictionary
+        Args
+            key
         """
         if key in self.cache_data.keys() and key:
             return self.cache_data[key]
